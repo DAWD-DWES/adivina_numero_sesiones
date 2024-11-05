267580b1e6b4583ab7168dc24e69d14210156407
jivanrod <jivanrod@gmail.com>
2024-11-05 20:21:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,17 +7,17 @@ define('LIM_SUP', 20);
 define('ERROR_APUESTA', 'Introduce una apuesta');
 
 if (filter_has_var(INPUT_POST, 'envio_apuesta')) {
+    $apuesta = filter_input(INPUT_POST, 'apuesta', FILTER_VALIDATE_INT);
     $numOculto = $_SESSION['num_oculto'];
     $numIntentos = $_SESSION['num_intentos'];
-    $apuesta = filter_input(INPUT_POST, 'apuesta', FILTER_VALIDATE_INT);
-    $numeros = $_SESSION['numeros'];
     $apuestaErr = empty($apuesta);
     if (!$apuestaErr) {
-        $numeros[] = $apuesta;
         $numIntentos++;
-        $_SESSION['apuesta'] = $apuesta;
         $_SESSION['num_intentos'] = $numIntentos;
+        $numeros = $_SESSION['numeros'];
+        $numeros[] = $apuesta;
         $_SESSION['numeros'] = $numeros;
+        $_SESSION['apuesta'] = $apuesta;
         $fin = $numIntentos >= MAX_INTENTOS || $apuesta === $numOculto; // Establezco si se ha acabado la partida o no
         $_SESSION['fin'] = $fin;
     }
@@ -27,11 +27,12 @@ if (filter_has_var(INPUT_POST, 'envio_apuesta')) {
     $numIntentos = $_SESSION['num_intentos'];
     $numeros = $_SESSION['numeros'];
     $fin = $_SESSION['fin'];
-    $apuesta = $_SESSION['apuesta'];
+    $apuesta = $_SESSION['apuesta'] ?? '';
 } else { // Si se arranca el juego o se solicita una nueva partida
     $_SESSION['num_intentos'] = $numIntentos = 0;
     $_SESSION['num_oculto'] = $numOculto = mt_rand(LIM_INF, LIM_SUP); // Genero un valor aleatorio
-    $_SESSION['numeros'] = $numeros = []; // Array de números jugados
+    $_SESSION['numeros'] = []; // Array de números jugados
+    $_SESSION['fin'] = false;
 }
 ?>
 
@@ -57,34 +58,38 @@ if (filter_has_var(INPUT_POST, 'envio_apuesta')) {
                             <?= ERROR_APUESTA ?>
                         </span>
                     </div>
-                    <?php if (isset($fin) && $fin): ?> <!-- Si no se ha acabado la partida incluyo la pista para el jugador -->
+                    <?php if ($fin ?? false): ?> <!-- Si se ha acabado la partida $fin está definido y es verdadero -->
                         <div class="submit-seccion">
-                            <!-- Si se ha acabado el juego añado un botón para iniciar una nueva partida y un mensaje de fin de juego -->
+                            <!-- Añado un botón para iniciar una nueva partida y un mensaje de fin de juego -->
                             <input class="submit" type="submit" 
                                    value="Nuevo Juego" name="nuevo_juego" /> 
                         </div>
-                        <p class="info-seccion"><?=
-                            ($apuesta === $numOculto) ?
-                                    "Enhorabuena!!! El número era el $numOculto. Lo has acertado en $numIntentos " . (($numIntentos !== 1) ?
-                                            "intentos" : "intento") : "Lo sentimos!!. El número era $numOculto"
-                            ?></p> 
-                    <?php else: ?>
+                        <?php if (!isset($petNumerosJugados)): ?> <!-- Si no se solicita los números jugados muestro un mensaje de fin de juego -->
+                            <p class="info-seccion"><?=
+                                ($apuesta === $numOculto) ?
+                                        "Enhorabuena!!! El número era el $numOculto. Lo has acertado en $numIntentos " . (($numIntentos !== 1) ?
+                                                "intentos" : "intento") : "Lo sentimos!!. El número era $numOculto"
+                                ?></p>
+                        <?php endif ?>
+                    <?php endif ?>
+                        <?php if (!($fin ?? false)): ?> <!-- Si no se ha acabado la partida $fin está definido y es falso o no está definido --> 
                         <div class="submit-seccion">
-                            <!-- Si es el inicio del juego o no se ha acabado el juego añado un botón para enviar apuesta -->
+                            <!-- Añado un botón para enviar apuesta -->
                             <input class="submit" type="submit" 
                                    value="Realiza una apuesta" name="envio_apuesta" /> 
                         </div>
-                        <?php if (isset($fin) && !$fin): ?>
-                            <div class="info-seccion">
-                                <!-- Si no se ha acabado el juego añado una pista para el usuario -->
-                                <p>Intentos restantes: <?= MAX_INTENTOS - $numIntentos ?></p>
+                        <div class="info-seccion">
+                            <!-- Muestro los intentos restantes -->
+                            <p>Intentos restantes: <?= MAX_INTENTOS - $numIntentos ?></p>
+                            <?php if (!empty($apuesta) && !isset($petNumerosJugados)): ?> <!-- Si apuesta no está vacío y no se solicitan los números jugados muestro una pista -->  
                                 <p><?= ($apuesta <=> $numOculto) > 0 ? 'Inténtalo con un número mas bajo' : 'Inténtalo con un número mas alto' ?></p>
-                            </div>
-                        <?php endif ?>
-                    <?php endif ?> 
-                    <?php if (isset($petNumerosJugados)): ?>
+                            <?php endif ?>
+                        </div>
+                    <?php endif ?>
+                    <?php if (isset($petNumerosJugados)): ?> <!-- Si es una solicitud de números jugados se muestran los números -->
                         <p class="info-seccion">
-                            <?= ($numeros) ? "Ya has jugado con los siguientes números: " . implode(",", $numeros) : "No hay números todavía" ?></p>
+                            <?= ($numeros) ? "Ya has jugado con los siguientes números: " . implode(",", $numeros) : "No hay números todavía" ?>
+                        </p>
                     <?php endif ?>
                     <div class="submit-seccion">
                         <!-- Si es el inicio del juego o no se ha acabado el juego añado un botón para enviar apuesta -->
